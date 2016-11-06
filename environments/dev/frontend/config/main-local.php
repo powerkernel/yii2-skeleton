0fd8ad23c51e4ca4844751d10102422e38f9f6da
ALIENWARE\Harry <harry@modernkernel.com>
2016-11-06 19:03:49 +0700

    enable allowed ip in debug module

--- environments/dev/frontend/config/main-local.php
+++ environments/dev/frontend/config/main-local.php
@@ -14,6 +14,7 @@ if (!YII_ENV_TEST) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
+        'allowedIPs' => []
     ];
 
     $config['bootstrap'][] = 'gii';
