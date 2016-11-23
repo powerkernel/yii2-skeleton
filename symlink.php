8d5d8abe97b6fc23e00207b34c75f8016e0f595e
ALIENWARE\Harry <harry@modernkernel.com>
2016-11-23 19:35:06 +0700

    update symlink cmd

--- symlink.php
+++ symlink.php
@@ -5,9 +5,13 @@
 
 $faviconLink = __DIR__ . '/backend/web/favicon';
 $imageLink = __DIR__ . '/backend/web/images';
+$backendLink = __DIR__ . '/frontend/web/backend';
 if (!is_link($faviconLink)) {
     symlink(__DIR__ . '/frontend/web/favicon', $faviconLink);
 }
 if (!is_link($imageLink)) {
     symlink(__DIR__ . '/frontend/web/images', $imageLink);
+}
+if (!is_link($backendLink)) {
+    symlink(__DIR__ . '/backend/web', $backendLink);
 }
