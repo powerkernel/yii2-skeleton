9a2f55a79d625be74cd779f03ca90df0d193af0e
ALIENWARE\Harry <harry@modernkernel.com>
2017-02-13 14:10:31 +0700

    view banner

--- backend/views/banner/view.php
+++ backend/views/banner/view.php
@@ -23,29 +23,37 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="banner-view">
     <div class="box box-info">
         <div class="box-body">
-            <div class="table-responsive">
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'title',
-            'text_content:ntext',
-            'link_url:url',
-            'link_option',
-            'status',
-            'created_at',
-            'updated_at',
-        ],
-    ]) ?>
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="table-responsive">
+                        <?= DetailView::widget([
+                            'model' => $model,
+                            'attributes' => [
+                                'id',
+                                'title',
+                                'text_content:html',
+                                'link_url:url',
+                                'link_option',
+                                ['attribute' => 'status', 'value' => $model->statusText],
+                                'created_at:dateTime',
+                                'updated_at:dateTime',
+                            ],
+                        ]) ?>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <?= Html::img($model->banner_url, ['alt' => $model->title, 'class'=>'img-responsive img-thumbnail']) ?>
+                </div>
             </div>
+
             <p>
                 <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
                 <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
-                'class' => 'btn btn-danger',
-                'data' => [
-                'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
-                'method' => 'post',
-                ],
+                    'class' => 'btn btn-danger',
+                    'data' => [
+                        'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
+                        'method' => 'post',
+                    ],
                 ]) ?>
             </p>
         </div>
