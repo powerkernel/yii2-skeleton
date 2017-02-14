c45c76a7df6bfbe546f037b08e714541e9e61205
ALIENWARE\Harry <harry@modernkernel.com>
2017-02-14 17:07:03 +0700

    Banner: improve

--- backend/views/banner/view.php
+++ backend/views/banner/view.php
@@ -31,6 +31,7 @@ $this->params['breadcrumbs'][] = $this->title;
                             'attributes' => [
                                 'id',
                                 'title',
+                                'lang',
                                 'text_content:html',
                                 'link_url:url',
                                 'link_option',
