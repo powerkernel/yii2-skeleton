351f0ccfc78573efaef9da7e778053d0937d1bb9
ALIENWARE\Harry <harry@modernkernel.com>
2017-03-08 19:41:18 +0700

    enable show/hide powered by

--- common/config/params.php
+++ common/config/params.php
@@ -4,5 +4,6 @@ return [
     'version'=>'0.2dev',
     'releaseDate'=>mktime(12,0,0, 11,16,2016),
     'sitemapPageSize'=>200,
-    'enableBlog'=>true
+    'enableBlog'=>true,
+    'showPowered'=>true
 ];
--- frontend/widgets/views/footer.php
+++ frontend/widgets/views/footer.php
@@ -1,8 +1,10 @@
 <footer class="main-footer">
     <div class="container">
+        <?php if(Yii::$app->params['showPowered']===true):?>
         <div class="pull-right hidden-xs">
             <?= \common\Core::powered() ?>
         </div>
+        <?php endif;?>
         <strong><?= Yii::t('app', 'Copyright') ?> &copy; <?= date('Y') ?> <?= Yii::$app->name ?>.</strong> <?= Yii::t('app', 'All rights reserved.') ?>
     </div>
 </footer>
