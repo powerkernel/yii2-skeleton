8ee9940b9bcacced53517539be25d79aec3ae8e5
harrytang <harry@modernkernel.com>
2017-05-04 17:09:03 +0700

    update 7z cmd

--- 7z.cmd
+++ 7z.cmd
@@ -1,1 +1,1 @@
-"C:\Program Files\7-Zip\7z.exe" a upload.zip * -r -x!.git -x!.idea -x!.data -x!.ssl -x!localhost.php -x!index-test.php -x!backend\web\images -x!backend\web\favicon
+"C:\Program Files\7-Zip\7z.exe" a upload.zip * -r -x!.git -x!.idea -x!.data -x!.ssl -x!localhost.php -x!index-test.php -x!frontend\web\backend -x!backend\web\images -x!backend\web\favicon -x!backend\web\css
