601ea30aeab85fedf84e39779dd3063af6f99db9
harrytang <harry@modernkernel.com>
2017-08-01 02:56:47 +0700

    remove version info

--- common/config/params.php
+++ common/config/params.php
@@ -1,17 +1,11 @@
 <?php
 return [
     'rootAdmin'=>[1],
-    'version'=>'0.3.1dev',
-    'releaseDate'=>mktime(12,0,0, 06,30,2017),
     'sitemapPageSize'=>200,
     'enableBlog'=>true,
     'showPowered'=>true,
-
     'mongodb'=>[
-        'i18n'=>false, // set i18n uses mongodb,
-        'taskLog'=>false
+        'i18n'=>false,
+        'taskLog'=>false,
     ]
-
-
-
 ];
