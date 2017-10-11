a1d643f3b034ce9b618f84260e15a9ad4c41f2c8
Harry Tang <harry@powerkernel.com>
2017-10-11 17:25:27 +0700

    update author info

--- common/layouts/base.php
+++ common/layouts/base.php
@@ -18,7 +18,7 @@ use yii\helpers\Html;
 +++++++++++++++++++++++++++++++++++++++++++
 @author Harry Tang <harry@powerkernel.com>
 @link https://powerkernel.com
-@copyright Copyright (c) 2016 Modern Kernel
+@copyright Copyright (c) 2017 Power Kernel
 +++++++++++++++++++++++++++++++++++++++++++
 -->
 <head>
