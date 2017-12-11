2be9312f0a5402722d1d7af8bb97d19cc766d533
Harry Tang <harry@powerkernel.com>
2017-12-11 12:10:14 +0700

    add export languages funtion

--- backend/views/i18n/index.php
+++ backend/views/i18n/index.php
@@ -10,9 +10,10 @@ use yii\widgets\Pjax;
 /* @var $searchModel \common\models\MessageSearch|\common\models\mongodb\MessageSearch */
 /* @var $languages [] */
 /* @var $mongodb boolean */
+/* @var $availableExport [] */
 
 
-$this->title = Yii::t('app', 'Translation');
+$this->title = Yii::t('app', 'Languages');
 $keywords = '';
 $description = '';
 
@@ -52,68 +53,103 @@ $this->registerCss($css);
 
 ?>
 <div class="i18n-message-index">
-    <div class="box box-primary">
-        <div class="box-body">
-            <?php Pjax::begin(['id' => 'message-translation-wrap']); ?>
-            <?= GridView::widget([
-                'id' => 'i18n-message-grid',
-                'options' => ['class' => 'grid-view table-responsive'],
-                'dataProvider' => $dataProvider,
-                'filterModel' => $searchModel,
-                'columns' => [
-                    ['class' => 'yii\grid\SerialColumn'],
-                    [
-                        'attribute' => 'language',
-                        'value' => 'language',
-                        'filter' => $mongodb ? \common\models\mongodb\Message::getLocaleList() : \common\models\Message::getLocaleList([Yii::$app->sourceLanguage])
-                    ],
-                    [
-                        'attribute' => 'category',
-                        'value' => $mongodb ? 'category' : 'source.category',
-                        'filter' => $mongodb ? \common\models\mongodb\Message::getCategoryList() : \common\models\SourceMessage::getCategoryList()
-                    ],
-                    ['attribute' => 'message', 'value' => $mongodb ? 'message' : 'source.message'],
-                    ['attribute' => 'translation', 'format' => 'raw', 'value' => function ($model) {
-                        return \powerkernel\jeditable\Editable::widget([
-                            'content' => strip_tags($model->translation),
-                            'saveUrl' => Yii::$app->urlManager->createUrl(['/i18n/save-translation']),
-                            'options' => ['id' => 'message_' . $model->id . '_' . $model->language, 'class' => 'jeditable-text'],
-                            'clientOptions' => [
-                                'tooltip' => Yii::t('app', 'Click to edit'),
-                                'indicator' => Yii::t('app', 'Saving...'),
-                                'width' => '93%',
-                            ]
-                        ]);
-                    },
-                        //'contentOptions'=>['class'=>'jeditable-text']
-                    ],
-                    ['attribute' => 'is_translated', 'value' => function ($model) {
-                        return Core::getYesNoText($model->is_translated);
-                    }, 'filter' => Core::getYesNoOption()],
-                    [
-                        'class' => 'yii\grid\ActionColumn',
-                        'template' => '{delete}'
-                        //'contentOptions'=>['style'=>'min-width: 70px']
-                    ],
-                ],
-            ]); ?>
-            <?php Pjax::end(); ?>
-            <div>
-                <?= \yii\helpers\Html::beginForm(Yii::$app->urlManager->createUrl(['/i18n/add']), 'post', ['class' => 'form-inline']) ?>
-                <div class="form-group">
-                    <label for="language"><?= Yii::t('app', 'Language') ?></label>
-                    <?= \yii\helpers\Html::dropDownList('language', null, $languages, ['class' => 'form-control']) ?>
+    <div class="nav-tabs-custom">
+        <ul class="nav nav-tabs">
+            <li class="active"><a href="#translation" data-toggle="tab" aria-expanded="true"><?= Yii::t('app', 'Translation') ?></a></li>
+            <li class=""><a href="#export" data-toggle="tab" aria-expanded="false"><?= Yii::t('app', 'Export') ?></a></li>
+            <li class=""><a href="#import" data-toggle="tab" aria-expanded="false"><?= Yii::t('app', 'Import') ?></a></li>
+        </ul>
+        <div class="tab-content">
+            <div class="tab-pane active" id="translation">
+                <div class="box box-widget" style="box-shadow: none">
+                    <div class="box-body">
+                    <?php Pjax::begin(['id' => 'message-translation-wrap']); ?>
+                    <?= GridView::widget([
+                        'id' => 'i18n-message-grid',
+                        'options' => ['class' => 'grid-view table-responsive'],
+                        'dataProvider' => $dataProvider,
+                        'filterModel' => $searchModel,
+                        'columns' => [
+                            ['class' => 'yii\grid\SerialColumn'],
+                            [
+                                'attribute' => 'language',
+                                'value' => 'language',
+                                'filter' => $mongodb ? \common\models\mongodb\Message::getLocaleList() : \common\models\Message::getLocaleList([Yii::$app->sourceLanguage])
+                            ],
+                            [
+                                'attribute' => 'category',
+                                'value' => $mongodb ? 'category' : 'source.category',
+                                'filter' => $mongodb ? \common\models\mongodb\Message::getCategoryList() : \common\models\SourceMessage::getCategoryList()
+                            ],
+                            ['attribute' => 'message', 'value' => $mongodb ? 'message' : 'source.message'],
+                            ['attribute' => 'translation', 'format' => 'raw', 'value' => function ($model) {
+                                return \powerkernel\jeditable\Editable::widget([
+                                    'content' => strip_tags($model->translation),
+                                    'saveUrl' => Yii::$app->urlManager->createUrl(['/i18n/save-translation']),
+                                    'options' => ['id' => 'message_' . $model->id . '_' . $model->language, 'class' => 'jeditable-text'],
+                                    'clientOptions' => [
+                                        'tooltip' => Yii::t('app', 'Click to edit'),
+                                        'indicator' => Yii::t('app', 'Saving...'),
+                                        'width' => '93%',
+                                    ]
+                                ]);
+                            },
+                                //'contentOptions'=>['class'=>'jeditable-text']
+                            ],
+                            ['attribute' => 'is_translated', 'value' => function ($model) {
+                                return Core::getYesNoText($model->is_translated);
+                            }, 'filter' => Core::getYesNoOption()],
+                            [
+                                'class' => 'yii\grid\ActionColumn',
+                                'template' => '{delete}'
+                                //'contentOptions'=>['style'=>'min-width: 70px']
+                            ],
+                        ],
+                    ]); ?>
+                    <?php Pjax::end(); ?>
+                    <div>
+                        <?= \yii\helpers\Html::beginForm(Yii::$app->urlManager->createUrl(['/i18n/add']), 'post', ['class' => 'form-inline']) ?>
+                        <div class="form-group">
+                            <label for="language"><?= Yii::t('app', 'Language') ?></label>
+                            <?= \yii\helpers\Html::dropDownList('language', null, $languages, ['class' => 'form-control']) ?>
+                        </div>
+                        <button type="submit" class="btn btn-success"><?= Yii::t('app', 'Add') ?></button>
+                        <?= \yii\helpers\Html::endForm() ?>
+                    </div>
                 </div>
-                <button type="submit" class="btn btn-success"><?= Yii::t('app', 'Add') ?></button>
-                <?= \yii\helpers\Html::endForm() ?>
+                <!-- Loading (remove the following to stop the loading)-->
+                <div class="overlay grid-view-overlay hidden">
+                    <i class="fa fa-refresh fa-spin"></i>
+                </div>
+                </div>
             </div>
-        </div>
-        <!-- Loading (remove the following to stop the loading)-->
-        <div class="overlay grid-view-overlay hidden">
-            <i class="fa fa-refresh fa-spin"></i>
-        </div>
-        <!-- end loading -->
 
+
+            <div class="tab-pane" id="export">
+                <div class="row">
+                <?php foreach($availableExport as $export):?>
+                    <div class="col-sm-4">
+                        <div class="box box-primary box-address">
+                            <div class="box-header with-border">
+                                <div class="text-uppercase"><strong><?= $export['title'] ?></strong></div>
+                            </div>
+                            <div class="box-body">
+                                <?php foreach ($export['cats'] as $cat):?>
+                                    <a href="<?= Yii::$app->urlManager->createUrl(['i18n/export', 'lang'=>$export['lang'], 'cat'=>$cat]) ?>" class="label label-primary"><?= $cat ?></a>
+                                <?php endforeach;?>
+                            </div>
+                        </div>
+                    </div>
+                <?php endforeach;?>
+                </div>
+            </div>
+
+            <div class="tab-pane" id="import">
+                Import languages coming soon!
+            </div>
+
+        </div>
+        <!-- /.tab-content -->
     </div>
 
 
