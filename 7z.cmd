5c6deffa9132d8350d30fa12ccf3ff4a9f9047e7
Harry Tang <harry@powerkernel.com>
2018-03-31 08:16:41 +0700

    update findIdentity function

--- 7z.cmd
+++ 7z.cmd
@@ -1,1 +1,1 @@
-"C:\Program Files\7-Zip\7z.exe" a upload.zip * -r -x!.git -x!.idea -x!.data -x!.ssl -x!localhost.php -x!index-test.php -x!frontend\web\backend -x!backend\web\images -x!backend\web\favicon -x!backend\web\css -x!frontend/web/index.php -x!backend/web/index.php -x!yii
+"C:\Program Files\7-Zip\7z.exe" a upload.zip * -r -x!.git -x!.idea -x!.data -x!.ssl -x!localhost.php -x!index-test.php -x!frontend\web\backend -x!backend\web\images -x!backend\web\favicon -x!backend\web\css -x!frontend/web/index.php -x!backend/web/index.php -x!grunt-email-workflow -x!yii
