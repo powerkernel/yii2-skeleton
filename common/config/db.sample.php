029c6b9fa0bb268001cc389b0e3155b4ef6dceb5
Harry Tang <harry@powerkernel.com>
2018-12-16 23:08:08 +0200

    Create db.sample.php

---
+++ common/config/db.sample.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'dsn' => 'DB_DNS',
+    'options' => [
+        'username' => 'DB_USER',
+        'password' => 'DB_PASS',
+        'ssl' => true,
+        'replicaSet' => 'DB_REPSET',
+        'authSource' => 'admin'
+    ]
+];
