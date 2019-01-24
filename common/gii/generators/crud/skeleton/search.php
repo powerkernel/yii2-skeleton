58920a0c64aa1b9b7bedf7bbafec48c55b2fe93f
Harry Tang <harry@powerkernel.com>
2019-01-24 18:14:52 +0200

    Update search.php

--- common/gii/generators/crud/skeleton/search.php
+++ common/gii/generators/crud/skeleton/search.php
@@ -95,12 +95,11 @@ class <?= $searchModelClass ?> extends <?= isset($modelAlias) ? $modelAlias : $m
         // grid filtering conditions
         <?= implode("\n        ", $searchConditions) ?>
 
-        //if(!empty($this->created_at)){
+        //if (!empty($this->created_at)) {
         //    $query->andFilterWhere([
-        //        'DATE(CONVERT_TZ(FROM_UNIXTIME(`created_at`), :UTC, :ATZ))' => $this->created_at,
-        //    ])->params([
-        //        ':UTC'=>'+00:00',
-        //        ':ATZ'=>date('P')
+        //        'created_at' => ['$gte' => new UTCDateTime(strtotime($this->created_at) * 1000)],
+        //    ])->andFilterWhere([
+        //        'created_at' => ['$lt' => new UTCDateTime((strtotime($this->created_at) + 86400) * 1000)],
         //    ]);
         //}
 
