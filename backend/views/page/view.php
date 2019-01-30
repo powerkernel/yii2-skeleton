4f31aaaaa33a86bc28d9fddb9a0d2de5012b44ff
Harry Tang <harry@powerkernel.com>
2019-01-30 16:21:10 +0200

    Update view.php

--- backend/views/page/view.php
+++ backend/views/page/view.php
@@ -1,47 +1,83 @@
 <?php
 
-use yii\helpers\Html;
+use common\Core;
+use common\models\PageData;
+use powerkernel\tinymce\TinyMce;
+use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\PageData */
+/* @var $form yii\widgets\ActiveForm */
+/* @var $languages [] */
 
-$this->title = $model->title;
+
+$this->title = Yii::t('app', 'Update: {TITLE}', ['TITLE' => $model->title]);
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Pages'), 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
+$this->params['breadcrumbs'][] = ['label' => $model->title, 'url' => ['view', 'id_page' => $model->slug, 'language' => $model->language]];
+$this->params['breadcrumbs'][] = Yii::t('app', 'Update');
 
-//$js=file_get_contents(__DIR__.'/view.min.js');
+//$js = file_get_contents(__DIR__ . '/_form.min.js');
 //$this->registerJs($js);
-//$css=file_get_contents(__DIR__.'/view.css');
-//$this->registerJs($css);
+//$css=file_get_contents(__DIR__.'/update.css');
+//$this->registerCss($css);
 ?>
-<div class="page-data-view">
-    <div class="box box-info">
-        <div class="box-header with-border">
-            <h3 class="box-title"><?= Html::encode($this->title) ?></h3>
-        </div>
-        <div class="box-body">
-            <?php if ($model->page->show_description): ?>
-                <div><p><?= Html::encode($model->description) ?></p></div>
-            <?php endif; ?>
-            <div><?= $model->content ?></div>
-        </div>
-        <?php if ($model->page->show_update_date): ?>
-            <div class="box-footer">
-                <div class="pull-right font-light text-sm">
-                    <?= Yii::t('app', 'Last updated: {DATE}', ['DATE' => Yii::$app->formatter->asDate($model->updatedAt)]) ?>
-                </div>
-            </div>
-        <?php endif; ?>
-    </div>
+<div class="page-update">
 
-    <p>
-        <?= Html::a(Yii::t('app', 'Update'), ['update', 'slug' => $model->slug, 'language' => $model->language], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'slug' => $model->slug, 'language' => $model->language], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => Yii::t('app', 'Are you sure you want to delete this page?'),
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
+
+    <div class="box box-primary">
+        <div class="box-body">
+            <div class="page-data-form">
+                <?php $form = ActiveForm::begin(); ?>
+                <?= $form->field($model, 'slug')->textInput(['readonly' => true]) ?>
+                <?= $form->field($model, 'language')->textInput(['maxlength' => true, 'readonly' => true]) ?>
+                <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+                <?= $form->field($model, 'description')->textarea(['rows' => 3]) ?>
+
+                <div class="nav-tabs-custom">
+                    <ul class="nav nav-tabs">
+                        <li class="active"><a href="#tab_content" data-toggle="tab" aria-expanded="true"><?= $model->getAttributeLabel('content') ?></a></li>
+                        <li class=""><a href="#tab_photo_uploader" data-toggle="tab" aria-expanded="false"><?= Yii::t('app', 'Photo Uploader') ?></a></li>
+                    </ul>
+                    <div class="tab-content">
+                        <div class="tab-pane active" id="tab_content">
+                            <?= $form->field($model, 'content')->widget(TinyMce::class) ?>
+                        </div>
+                        <!-- /.tab-pane -->
+                        <div class="tab-pane" id="tab_photo_uploader">
+                            <?= \common\widgets\FlickrUploadWidget::widget() ?>
+                        </div>
+                        <!-- /.tab-pane -->
+                    </div>
+                    <!-- /.tab-content -->
+                </div>
+
+                <?= $form->field($model, 'keywords')->textInput(['maxlength' => true]) ?>
+                <?= $form->field($model, 'thumbnail')->textInput(['maxlength' => false, 'placeholder' => Yii::t('app', 'Must be at least 160x90 pixels and at most 1920x1080 pixels')]) ?>
+                <?= $form->field($model, 'status')->dropDownList(PageData::getStatusOption()) ?>
+                <?= $form->field($model->page, 'show_description')->dropDownList(Core::getYesNoOption()) ?>
+                <?= $form->field($model->page, 'show_update_date')->dropDownList(Core::getYesNoOption()) ?>
+                <div class="form-group">
+                    <p class="pull-left">
+                        <?= \common\components\SubmitButton::widget(['text'=>$model->scenario == 'create' ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), 'options'=>['class' => $model->scenario == 'create' ? 'btn btn-success' : 'btn btn-primary']]) ?>
+                    </p>
+                    <?php if (count($languages) > 0): ?>
+                        <div class="btn-group pull-right">
+                            <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown"
+                                    aria-haspopup="true" aria-expanded="false">
+                                <?= Yii::t('app', 'Languages') ?> <span class="caret"></span>
+                            </button>
+                            <ul class="dropdown-menu">
+                                <?php foreach ($languages as $key => $language): ?>
+                                    <li>
+                                        <a href="<?= Yii::$app->urlManager->createUrl(['/page/add-language', 'slug' => $model->slug, 'from' => $model->language, 'to' => $key]) ?>"><?= Yii::t('app', 'Add') ?> <?= $language ?></a>
+                                    </li>
+                                <?php endforeach; ?>
+                            </ul>
+                        </div>
+                    <?php endif; ?>
+                </div>
+                <?php ActiveForm::end(); ?>
+            </div>
+        </div>
+    </div>
 </div>
